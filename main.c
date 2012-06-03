11a88fbe8e58b169813c0b2d7b08d1318a7b0e42
Freehan <freehan@cs.ucla.edu>
2012-06-02 21:26:07 -0700

    commit before merge

--- main.c
+++ main.c
@@ -71,7 +71,7 @@ void check_repo_log(char *dir, char * slice)
 		{
 			get_file(dir, local_file_name, slice);
 		}
-//		else
+//		else  // local_seq_no == repo_seq_no ---> conflict resolving
 //		{
 //
 //		}
@@ -145,7 +145,11 @@ int main(int argc, char const *argv[]) {
 	int found = 0;
 
 	fp = fopen("binding.txt", "r");
-
+	if(fp == NULL)
+	{
+		printf("cannot find binding.txt\n");
+		exit(1);
+	}
 	while (fscanf(fp, "%s %s %s", dir_name, slice_name, group_key) != EOF) {
 		if (strcmp(argv[1], dir_name) == 0) {
 			printf("dir_name = %s\n", dir_name);
